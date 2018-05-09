fc274ab83b73e4521d97ec97dc5765b6bd6e134a
Alex <zdyb647@hotmail.com>
2018-05-09 13:36:45 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const port = process.env.PORT || 3000;
+var indexRouter = require('./routes/index');
+var app = express();
+var {Food} = require('./models/food');
+var hbs = require('express-handlebars');
+
+// view engine setup
+app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'}));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+// hbs.registerPartials(__dirname + '/views/partials');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+
+// Food.find({}, (err,doc) => {
+//   totalFat = 0;
+//   doc.forEach((e) => {
+//     totalFat += e.fat;
+//   })
+// });
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
+
+
+module.exports = app;
